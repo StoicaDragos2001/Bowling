1dd5ef9ef4a1a25807b52f17995b82d9378d03df
StoicaDragos2001 <V3790542@L3700100793>
2023-01-18 17:21:58 +0200

    Add project files.

---
+++ Bowling/Game.cs
@@ -0,0 +1,43 @@
+﻿namespace Game
+{
+    public class Game
+    {
+        private int _frames;
+        private List<Tuple<int, int>> _scoreboard = new List<Tuple<int, int>>();
+        private int _buffer = -1;   // -1 for first opportunity in frame
+
+
+        public Game(int frames = 2)
+        {
+            if (frames < 1 && frames > 10)
+                throw new ArgumentOutOfRangeException("The number of frames must be between 1 and 10");
+            _frames = frames;
+        }
+
+        public void Roll(int pins)
+        {
+            if (_buffer == -1)
+            {
+                _buffer = pins;
+
+            }
+            else
+            {
+                _scoreboard.Add(Tuple.Create(_buffer, pins));
+                _buffer = -1;
+                _frames -= 1;
+            }
+            if (_frames == 0)
+                Score();
+        }
+
+        private void Score()
+        {
+            for (int i = 1; i <= _scoreboard.Count; i++)
+            {
+                Console.WriteLine(String.Format("Round {0}:", i));
+                Console.WriteLine(String.Format("{0} -> {1}", _scoreboard[i - 1], (_scoreboard[i - 1].Item1 + _scoreboard[i - 1].Item2)));
+            }
+        }
+    }
+}
---
+++ Bowling/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(2);
+            game.Roll(3);
+            game.Roll(4);
+        }
+    }
+}
