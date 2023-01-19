d948d6bc9bd47a35609ca414d44746d30b55815b
StoicaDragos2001 <V3790542@L3700100793>
2023-01-19 13:47:22 +0200

    Logic Updates

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -3,11 +3,13 @@
     public class Game
     {
         private int _frames;
-        private List<Tuple<int, int>> _scoreboard = new List<Tuple<int, int>>();
+        private List<Tuple<int, int>> _shots = new List<Tuple<int, int>>();
         private int _buffer = -1;   // -1 for first opportunity in frame
+        private int _totalScore = 0;
+        private bool _bonusRound = false;
 
 
-        public Game(int frames = 2)
+        public Game(int frames = 10)
         {
             if (frames < 1 && frames > 10)
                 throw new ArgumentOutOfRangeException("The number of frames must be between 1 and 10");
@@ -16,28 +18,53 @@
 
         public void Roll(int pins)
         {
-            if (_buffer == -1)
+            if(_frames == 0)
             {
-                _buffer = pins;
-
+                _shots.Add(Tuple.Create(pins, 0));
+                _bonusRound = true;
             }
             else
             {
-                _scoreboard.Add(Tuple.Create(_buffer, pins));
-                _buffer = -1;
-                _frames -= 1;
+                if (_buffer == -1)
+                {
+                    _buffer = pins;
+                }
+                else
+                {
+                    _shots.Add(Tuple.Create(_buffer, pins));
+                    _buffer = -1;
+                    _frames -= 1;
+                    Console.WriteLine(_frames);
+                }
             }
-            if (_frames == 0)
-                Score();
         }
 
-        private void Score()
+        public void Score()
         {
-            for (int i = 1; i <= _scoreboard.Count; i++)
+            int roundScore = 0;
+            int numberOfRounds = _shots.Count;
+
+            Console.WriteLine("Number of rounds: " + numberOfRounds);
+
+            if (_bonusRound)
             {
-                Console.WriteLine(String.Format("Round {0}:", i));
-                Console.WriteLine(String.Format("{0} -> {1}", _scoreboard[i - 1], (_scoreboard[i - 1].Item1 + _scoreboard[i - 1].Item2)));
+                numberOfRounds -= 1;
             }
+
+            for (int indexRound = 1; indexRound <= numberOfRounds; indexRound++)
+            {
+                Console.WriteLine(String.Format("Round {0}:", indexRound));
+                if (_shots[indexRound - 1].Item1 == 10)
+                    roundScore = _shots[indexRound - 1].Item1 + _shots[indexRound - 1].Item2 + _shots[indexRound].Item1;
+                else if (_shots[indexRound - 1].Item1 + _shots[indexRound - 1].Item2 == 10)
+                    roundScore = _shots[indexRound - 1].Item1 + _shots[indexRound - 1].Item2 + _shots[indexRound].Item1;
+                else
+                    roundScore = _shots[indexRound - 1].Item1 + _shots[indexRound - 1].Item2;
+                Console.WriteLine(String.Format("{0} -> {1}", _shots[indexRound - 1], roundScore));
+                _totalScore += roundScore;
+                roundScore = 0;
+            }
+            Console.WriteLine(String.Format("------> Total score: {0} <------", _totalScore));
         }
     }
 }
--- Bowling/Program.cs
+++ Bowling/Program.cs
@@ -5,10 +5,39 @@
         static void Main(string[] args)
         {
             var game = new Game();
-            game.Roll(1);
             game.Roll(2);
-            game.Roll(3);
             game.Roll(4);
+
+            game.Roll(4);
+            game.Roll(6);
+
+            game.Roll(3);
+            game.Roll(2);
+
+            game.Roll(10);
+            game.Roll(2);
+
+            game.Roll(3);
+            game.Roll(1);
+
+            game.Roll(10);
+            game.Roll(10);
+
+            game.Roll(10);
+            game.Roll(4);
+
+            game.Roll(5);
+            game.Roll(3);
+
+            game.Roll(10);
+            game.Roll(6);
+
+            game.Roll(5);
+            game.Roll(5);
+
+            game.Roll(8);
+
+            game.Score();
         }
     }
 }
