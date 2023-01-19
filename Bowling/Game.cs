b55f7904feb83b1a4c5a3f2009211156adb70c4c
StoicaDragos2001 <V3790542@L3700100793>
2023-01-19 14:25:36 +0200

    Structure and naming refactor

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -2,9 +2,9 @@
 {
     public class Game
     {
-        private int _frames;
-        private List<Tuple<int, int>> _shots = new List<Tuple<int, int>>();
-        private int _buffer = -1;   // -1 for first opportunity in frame
+        private int _framesLeft;
+        private List<Tuple<int, int>> _shotsPerFrame = new List<Tuple<int, int>>();
+        private int _firstShotBuffer = -1;   // -1 for first opportunity in frame
         private int _totalScore = 0;
         private bool _bonusRound = false;
 
@@ -13,56 +13,67 @@
         {
             if (frames < 1 && frames > 10)
                 throw new ArgumentOutOfRangeException("The number of frames must be between 1 and 10");
-            _frames = frames;
+            _framesLeft = frames;
         }
 
         public void Roll(int pins)
         {
-            if(_frames == 0)
+            if (_framesLeft == 0)
             {
-                _shots.Add(Tuple.Create(pins, 0));
+                _shotsPerFrame.Add(Tuple.Create(pins, 0));
                 _bonusRound = true;
             }
             else
             {
-                if (_buffer == -1)
+                if (_firstShotBuffer == -1)
                 {
-                    _buffer = pins;
+                    _firstShotBuffer = pins;
                 }
                 else
                 {
-                    _shots.Add(Tuple.Create(_buffer, pins));
-                    _buffer = -1;
-                    _frames -= 1;
-                    Console.WriteLine(_frames);
+                    _shotsPerFrame.Add(Tuple.Create(_firstShotBuffer, pins));
+                    _firstShotBuffer = -1;
+                    _framesLeft -= 1;
                 }
             }
         }
 
+        private int CalculateFrameScore(int roundIndex)
+        {
+            var currentFrame = _shotsPerFrame[roundIndex - 1];
+            int currentFrameFirstShot = currentFrame.Item1;
+            int currentFrameSecondShot = currentFrame.Item2;
+            int nextFrameFirstShot = _shotsPerFrame[roundIndex].Item1;
+            int roundScore = 0;
+            if (currentFrameFirstShot == 10)
+            {
+                roundScore = currentFrameFirstShot + currentFrameSecondShot + nextFrameFirstShot;
+            }
+            else if (currentFrameFirstShot + currentFrameSecondShot == 10)
+            {
+                roundScore = currentFrameFirstShot + currentFrameSecondShot + nextFrameFirstShot;
+            }
+            else
+            {
+                roundScore = currentFrameFirstShot + currentFrameSecondShot;
+            }
+            Console.WriteLine(String.Format("{0} -> {1}", currentFrame, roundScore));
+            return roundScore;
+        }
+
         public void Score()
         {
-            int roundScore = 0;
-            int numberOfRounds = _shots.Count;
-
-            Console.WriteLine("Number of rounds: " + numberOfRounds);
+            int numberOfRounds = _shotsPerFrame.Count;
 
             if (_bonusRound)
             {
                 numberOfRounds -= 1;
             }
 
-            for (int indexRound = 1; indexRound <= numberOfRounds; indexRound++)
+            for (int roundIndex = 1; roundIndex <= numberOfRounds; roundIndex++)
             {
-                Console.WriteLine(String.Format("Round {0}:", indexRound));
-                if (_shots[indexRound - 1].Item1 == 10)
-                    roundScore = _shots[indexRound - 1].Item1 + _shots[indexRound - 1].Item2 + _shots[indexRound].Item1;
-                else if (_shots[indexRound - 1].Item1 + _shots[indexRound - 1].Item2 == 10)
-                    roundScore = _shots[indexRound - 1].Item1 + _shots[indexRound - 1].Item2 + _shots[indexRound].Item1;
-                else
-                    roundScore = _shots[indexRound - 1].Item1 + _shots[indexRound - 1].Item2;
-                Console.WriteLine(String.Format("{0} -> {1}", _shots[indexRound - 1], roundScore));
-                _totalScore += roundScore;
-                roundScore = 0;
+                Console.WriteLine(String.Format("Round {0}:", roundIndex));
+                _totalScore += CalculateFrameScore(roundIndex);
             }
             Console.WriteLine(String.Format("------> Total score: {0} <------", _totalScore));
         }
